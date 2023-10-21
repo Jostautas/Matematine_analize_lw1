1bffb58833e81c5c858bd10c6ce69732ed75c90e
Tadas Baltrunas <tadas@baltrunas.lt>
2023-10-21 22:37:52 +0300

    modified GUI

--- main.py
+++ main.py
@@ -1,105 +1,65 @@
 import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
 import numpy as np
 import tkinter as tk
-from tkinter import Entry, Button, Label
+from tkinter import Entry, Button, Label, Frame
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.colors import ListedColormap
 
-entry_fields = []  # Define entry_fields only once
+# Define entryFields
+entryFields = []
 
 # Constants and colors
-N = 15
+# N = 30
 EPSILON = 1e-8
-STEP = 0.04
+# STEP = 0.03
 WINDOW = 2
-colors = ["red", "blue", "yellow", "green", "magenta", "brown"]
+colors = ["red", "blue", "yellow", "green", "magenta", "lime", "purple", "aqua"]
 
-# Function to update the constants
-def update_constants():
-    global N, EPSILON, STEP
-    N = int(entry_n.get())
-    EPSILON = float(entry_epsilon.get())
-    STEP = float(entry_step.get())
+# Function to erase leading zeros in the polynomial numbers
+def eraseLeadingZeros(PolynomlialNumbers):
+    return PolynomlialNumbers[next((i for i, x in enumerate(PolynomlialNumbers) if x != 0), 7):]
 
-def plot_polynomial_graph():
-    polynomial_numbers = [int(entry_fields[i].get()) for i in range(7)]
-    clean_polynomial_numbers = eraseLeadingZeros(polynomial_numbers)
-
-    update_constants()  # Update constants from input fields
-
-    x_points, y_points, convergence_points = findFunctionZeroes(
-        EPSILON, N, STEP, WINDOW, clean_polynomial_numbers)
-
-    color_array = getColorArrayForScatterPlot(x_points, y_points, convergence_points)
-
-    np_x_points = np.array(x_points)
-    np_y_points = np.array(y_points)
-
-    scatter_plot.set_offsets(np.column_stack((np_x_points, np_y_points)))
-    scatter_plot.set_array(np.array(color_array))
-    ax.relim()
-    ax.autoscale_view()
-    
-
-def eraseLeadingZeros(polynomialNumbers):
-    for index in range(7):
-        if polynomialNumbers[index] != 0:
-            return polynomialNumbers[index:7]
-
-
-def printFormula(PolynomialNumbers):
-    formulaString = ""
-    power = len(PolynomialNumbers) - 1
-    for number in PolynomialNumbers[:len(PolynomialNumbers) - 1]:
-        formulaString += f"{number}x^{power} + "
-        power -= 1
-    formulaString += f"{PolynomialNumbers[-1]}"
-    print("The formula:")
-    print(formulaString)
-
-
-def function(PolynomialNumbers, x):
+# Function to calculate the function value at a given point
+def function(PolynomlialNumbers, x):
     result = 0.0
-    power = len(PolynomialNumbers) - 1
-    for number in PolynomialNumbers[:len(PolynomialNumbers) - 1]:
+    power = len(PolynomlialNumbers) - 1
+    for number in PolynomlialNumbers[:len(PolynomlialNumbers) - 1]:
         result += number * x**power
         power -= 1
-    result += PolynomialNumbers[-1]
+    result += PolynomlialNumbers[-1]
     return result
 
-
-def functionDerivative(PolynomialNumbers, x): # num*6*x^5 + num*5*x^4 + num*4*x^3 + num*3*x^2 + num*2*x + num*x + 1
+# Function to calculate the derivative of the function
+def functionDerivative(PolynomlialNumbers, x):
     result = 0.0
-    power = len(PolynomialNumbers) - 1
-    for number in PolynomialNumbers[:len(PolynomialNumbers) - 2]:
+    power = len(PolynomlialNumbers) - 1
+    for number in PolynomlialNumbers[:len(PolynomlialNumbers) - 2]:
         result += number * power * x**(power-1)
         power -= 1
-    result += PolynomialNumbers[-2]
+    result += PolynomlialNumbers[-2]
     return result
 
-
+# Function for the Newton-Raphson iteration
 def newton(x, cleanPolynomialNumbers):
     return x - function(cleanPolynomialNumbers, x) / functionDerivative(cleanPolynomialNumbers, x)
 
-
+# Function to find function zeroes and convergence points
 def findFunctionZeroes(epsilon, n, step, window, cleanPolynomialNumbers):
-    # Points that converge to zero:
     xPoints = []
     yPoints = []
-
     convergencePoints = []
 
     y = window
-    while y >= -window:  # from top to bottom
+    while y >= -window:
         x = -window
-        while x <= window:  # from left to right
+        while x <= window:
             x1 = complex(x, y)
-            for a in range(n-1):  # loop for n iterations
+            for a in range(n - 1):
                 x0 = x1
                 x1 = newton(x0, cleanPolynomialNumbers)
             if abs((x0.real + x0.imag) - (x1.real + x1.imag)) < epsilon:
-                xPoints.append(x) # (x, y) is the point that converges to the function zero (the x0)
+                xPoints.append(x)
                 yPoints.append(y)
                 convPoint = complex(round(x0.real, 3), round(x0.imag, 3))
                 convergencePoints.append(convPoint)
@@ -108,104 +68,119 @@ def findFunctionZeroes(epsilon, n, step, window, cleanPolynomialNumbers):
 
     return xPoints, yPoints, convergencePoints
 
-
+# Function to assign colors for the scatter plot
 def getColorArrayForScatterPlot(xPoints, yPoints, convergencePoints):
-    points = list(zip(xPoints, yPoints, convergencePoints))  # points = [(x, y, (r,i))] (make an array of tuples)
-
-    # set() stores only unique elements
+    points = list(zip(xPoints, yPoints, convergencePoints))
     uniqueConvergencePoints = set(convergencePoints)
-    # convert back to list, so that we can index items:
     uniqueConvergencePoints = list(uniqueConvergencePoints)
-
-    # give a color to every point
     colorArray = []
     for point in points:
         convPoint = point[2]
-        for i in range(
-                len(uniqueConvergencePoints)):  # go through every of our convergencePoint and check which index of it corresponds to the convergent point in our points array
+        for i in range(len(uniqueConvergencePoints)):
             if convPoint == uniqueConvergencePoints[i]:
                 colorArray.append(colors[i])
                 break
-
     return colorArray
-
-
-def plot(xPoints, yPoints, colorArray):
-    npXPoints = np.array(xPoints)
-    npYPoints = np.array(yPoints)
-    plt.scatter(npXPoints, npYPoints, c=colorArray)
-    plt.show()
-
-def plot_polynomial_graph():
-    polynomial_numbers = [int(entry_fields[i].get()) for i in range(7)]
-    clean_polynomial_numbers = eraseLeadingZeros(polynomial_numbers)
-
-    x_points, y_points, convergence_points = findFunctionZeroes(
-        EPSILON, N, STEP, WINDOW, clean_polynomial_numbers)
-    
-    color_array = getColorArrayForScatterPlot(x_points, y_points, convergence_points)
-
-    np_x_points = np.array(x_points)
-    np_y_points = np.array(y_points)
-
-    plt.scatter(np_x_points, np_y_points, c=color_array)
-    plt.show()
 
 # Create the main window
 root = tk.Tk()
-root.title("Newton's Method Visualization")
+
+# Calculate the screen width and height for centering the window
+screenWidth = root.winfo_screenwidth()
+screenHeight = root.winfo_screenheight()
+windowWidth = 530  # Width of the window
+windowHeight = 630  # Height of the window
+x = (screenWidth - windowWidth) // 2
+y = (screenHeight - windowHeight) // 2 - 30
+root.geometry(f"{windowWidth}x{windowHeight}+{x}+{y}")
+
+root.title("Polynomial Plotter")  # Set the window title
 
 # Entry fields for polynomial parameters
-label = Label(root, text=f"POLYNOM", font="bald")
-label.grid(row=0, column=0)
+polynomialDescription = "POLYNOM\n\nax⁶  +  bx⁵  +  cx⁴  +  dx³  +  ex²  +  fx¹  +  g"
+label = Label(root, text=polynomialDescription, font=("Helvetica", 12, "bold"))
+label.grid(row=0, column=0, columnspan=2, pady=(10, 0))
 
-for i in range(7):
-    label = Label(root, text=f"Coefficient {i + 1}:")
-    label.grid(row=i + 1, column=0)
-    entry = Entry(root)
-    entry.grid(row=i + 1, column=1)
-    entry_fields.append(entry)
+coefficientsFrame = Frame(root)
+coefficientsFrame.grid(row=1, column=0, columnspan=2)
 
-label = Label(root, text=f"CONSTANTS", font="bald")
-label.grid(row=8, column=0)
+coefficients = ["a", "b", "c", "d", "e", "f", "g"]
 
-# Entry fields for constant values
-label_n = Label(root, text=f"N:")
-label_n.grid(row=9, column=0)
-entry_n = Entry(root)
-entry_n.grid(row=9, column=1)
-entry_n.insert(0, str(N))
+# Entry boxes for coefficient input
+entryFrame = Frame(root)
+entryFrame.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
+for i, coef in enumerate(coefficients):
+    label = Label(entryFrame, text=f"{coef}:", font=("Helvetica", 10))
+    label.grid(row=0, column=i)
+    entry = Entry(entryFrame, font=("Helvetica", 10), width=5)
+    entry.grid(row=1, column=i, padx=5)
+    entryFields.append(entry)
 
-label_epsilon = Label(root, text=f"EPSILON:")
-label_epsilon.grid(row=10, column=0)
-entry_epsilon = Entry(root)
-entry_epsilon.grid(row=10, column=1)
-entry_epsilon.insert(0, str(EPSILON))
+label = Label(root, text="CONSTANTS", font=("Helvetica", 12, "bold"))
+label.grid(row=8, column=0, columnspan=2)
 
-label_step = Label(root, text=f"STEP:")
-label_step.grid(row=11, column=0)
-entry_step = Entry(root)
-entry_step.grid(row=11, column=1)
-entry_step.insert(0, str(STEP))
+# Entry field for constant values (aligned to the middle)
+labelN = Label(root, text="N:", font=("Helvetica", 10))
+labelN.grid(row=9, column=0, padx=(0, 50), pady=5)
+entryN = Entry(root, font=("Helvetica", 10), width=5)
+entryN.grid(row=9, column=0, padx=(50, 0), pady=5)
 
-label = Label(root, text=f"")
-label.grid(row=12, column=0)
+labelStep = Label(root, text="STEP:", font=("Helvetica", 10))
+labelStep.grid(row=9, column=1, padx=(100, 150), pady=5)
+entryStep = Entry(root, font=("Helvetica", 10), width=5)
+entryStep.grid(row=9, column=1, padx=(150, 100), pady=5)
 
-# Button to plot the graph
-plot_button = Button(root, text="Plot Graph", command=plot_polynomial_graph)
-plot_button.grid(row=13, column=0, columnspan=2)
-
-# Create a Matplotlib Figure and an Axes object for the colorbar
-fig = Figure()
+# Create a Matplotlib Figure and an Axes object
+fig = Figure(figsize=(8, 8))
 ax = fig.add_subplot(111)
 
-# Create an initial scatter plot (empty)
-scatter_plot = ax.scatter([], [], c=[], cmap="jet")
+cmap = ListedColormap(colors)
+scatterPlot = ax.scatter([], [], c=[], cmap=cmap)
 ax.set_xlim(-WINDOW, WINDOW)
 ax.set_ylim(-WINDOW, WINDOW)
 
-# Create a colorbar
-colorbar = fig.colorbar(scatter_plot, ax=ax)
-colorbar.set_label("Convergence")
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvasWidget = canvas.get_tk_widget()
+canvasWidget.grid(row=12, column=0, columnspan=2, padx=10, pady=10)
+
+# Create a Text widget to display convergence points
+convergenceText = tk.Text(root, height=5, width=40, font=("Helvetica", 10))
+convergenceText.grid(row=13, column=0, columnspan=2, padx=10, pady=10)
+
+# Function to plot the polynomial graph
+def plotPolynomialGraph():
+    # Get the user-set value for N and STEP
+    n = int(entryN.get())
+    step = float(entryStep.get())
+    PolynomlialNumbers = [float(entryFields[i].get()) for i in range(7)]
+    cleanPolynomialNumbers = eraseLeadingZeros(PolynomlialNumbers)
+    xPoints, yPoints, convergencePoints = findFunctionZeroes(EPSILON, n, step, WINDOW, cleanPolynomialNumbers)
+    colorArray = getColorArrayForScatterPlot(xPoints, yPoints, convergencePoints)
+
+    colorNumericValues = [colors.index(color) for color in colorArray]
+
+    scatterPlot.set_offsets(np.column_stack((xPoints, yPoints)))
+    scatterPlot.set_array(np.array(colorNumericValues))
+    scatterPlot.set_clim(vmin=0, vmax=len(colors) - 1)
+
+    # Add the "x =" strings with convergence points
+    convergenceText.delete('1.0', tk.END)
+    convergenceText.insert(tk.END, "Convergence Points:\n")
+    for point in convergencePoints:
+        text = convergenceText.get("1.0", "end-1c")
+        if text.find(str(point)) == -1:
+            convergenceText.insert(tk.END, f"x = {point}\n")
+        else:
+            continue
+
+    canvas.draw()
+
+# Button to plot the graph
+plotButton = Button(root, text="Plot Graph", font=("Helvetica", 10, "bold"), command=plotPolynomialGraph)
+plotButton.grid(row=11, column=0, columnspan=2, padx=10, pady=10)
+
+# Configure row and column weights to make components expand with window size
+root.grid_rowconfigure(12, weight=1)
+root.grid_columnconfigure(0, weight=1)
 
 root.mainloop()
